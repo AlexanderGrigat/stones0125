c4b18dab50829937c635b320858cedb633afa70f
Alexander Grigat <alexander.grigat@sn-invent.de>
2025-01-09 13:25:05 +0100

    Aufgabe 6.2

---
+++ src/app/utils/validators/custom-validators.spec.ts
@@ -0,0 +1,7 @@
+import { CustomValidators } from './custom-validators';
+
+describe('CustumValidator', () => {
+  it('should create an instance', () => {
+    expect(new CustomValidators()).toBeTruthy();
+  });
+});
